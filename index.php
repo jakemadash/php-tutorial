c5f8ff83cbeb7e86c553c0276c2fa71845ea8feb
Jake Ashton <jakemaddenashton@gmail.com>
2024-07-15 08:32:33 -0700

    feat: add db connection

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 
 require('functions.php');
+
+$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4';
+$pdo = new PDO($dsn, 'root');
+$statement = $pdo->prepare('SELECT * from posts');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo $post['title'];
+}
+
 require('router.php');
